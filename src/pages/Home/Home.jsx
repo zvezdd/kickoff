dc3b4ca90fe44b0965e20c654deb21c370b0bf20
zvezdd <qwiszvezd@gmail.com>
2024-07-23 00:13:26 +0600

    added a loader from a ui library

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,27 +3,35 @@ import { getDocs, collection } from "firebase/firestore";
 import { db } from "../../config/firebase-config";
 import Post from "./Post";
 import styles from "./Home.module.css";
+import Loader from "../../components/Loader";
 
 export default function Home() {
   const [postsList, setPostsList] = useState([]);
   const postsRef = collection(db, "posts");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getPosts = async () => {
+      setLoading(true)
       const data = await getDocs(postsRef);
       setPostsList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setLoading(false)
     };
     getPosts();
   }, []);
 
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
+    <h1>Home</h1>
+    {loading ? ( // если идет загрузка, показываем индикатор загрузки
+      <Loader/>
+    ) : (
       <div className={styles.postsList}>
         {postsList.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
-    </div>
+    )}
+  </div>
   );
 }
