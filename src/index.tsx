cb9724a7f246915b7b1fa186d492bf830f6f6704
zvezdd <qwiszvezd@gmail.com>
2024-08-07 23:50:40 +0600

    redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import  store  from './redux/store'
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (rootElement !== null) {
   const root = ReactDOM.createRoot(rootElement as HTMLElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   );
 } else {
-  console.error('Root element not found');
+  console.error("Root element not found");
 }
