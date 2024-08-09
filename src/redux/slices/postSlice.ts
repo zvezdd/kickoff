5f7fdf1b47bfb835492c98e8239da5d8be70d5c4
zvezdd <qwiszvezd@gmail.com>
2024-08-09 23:25:35 +0600

    post-slice

--- src/redux/slices/postSlice.ts
+++ src/redux/slices/postSlice.ts
@@ -10,22 +10,16 @@ const initialState: CounterState = {
 }
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: 'post',
   initialState,
   reducers: {
     increment: (state) => {
       state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    },
+    }
   },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment } = counterSlice.actions
 
 
 export default counterSlice.reducer
